4ad9a374e643bc1b522efeea5ab229a956c905e6
Jonas Fortes <jonascforte@hotmail.com>
2023-05-18 15:39:50 -0300

    Ideia inicial para aleatorização das posições iniciais de alunos e bugs

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Plano p = new Plano(6, 6);
+		
+		ServicosJogo s = new ServicosJogo(p, 10, 10);
+		
+		ArrayList<int[]> posicoesAlunos = s.sortearAlunosNoPlano();
+		
+		for(int[] posicao : posicoesAlunos) {
+			System.out.println(posicao[0]);
+			System.out.println(posicao[1]);
+			System.out.println("---------------");
+		}
 		
 
 	}
--- src/ServicosJogo.java
+++ src/ServicosJogo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ServicosJogo {
 	
@@ -13,14 +15,53 @@ public class ServicosJogo {
 		this.qtdBugs = qtdBugs;
 	}
 
-
-	public static void sortearElementos() {
+	
+	private int sortearNumero(int min, int max) {
+		Random random = new Random();
+		
+		return random.nextInt((max - min) + 1) + min;
+	}
+	
+	
+	public ArrayList<int[]> sortearAlunosNoPlano() {	
+		ArrayList<int[]> posicoesIniciaisAlunos = new ArrayList<int[]>();
+		
+		for(int i=1; i<=this.qtdAlunos; i++) {
+			int[] coordenada = new int[2];
+			coordenada[0] = sortearNumero(1, this.plano.tamanhoX);
+			coordenada[1] = sortearNumero(1, this.plano.tamanhoY);
+			
+			for(int[] c : posicoesIniciaisAlunos) {
+				while(coordenada[0] == c[0] && coordenada[1] == c[1]) {
+					coordenada[0] = sortearNumero(1, this.plano.tamanhoX);
+					coordenada[1] = sortearNumero(1, this.plano.tamanhoY);
+				}
+			}
+			
+			posicoesIniciaisAlunos.add(coordenada);
+				
+		}
+		return posicoesIniciaisAlunos;
+	}
+	
+	
+	public static void sortearBugsNoPlano( ) {
+		
 		
 	}
 	
-	public static void povoarPlano( ) {
+	
+	
+	public void povoarPlano() {
 		
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
